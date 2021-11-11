8fbf3569cf1e0df3356b052c4cebe9fb9bef05b7
ZakOpry <88099668+ZakOpry@users.noreply.github.com>
2021-11-11 12:38:23 -0500

    Added function to redirect after registering and logging in

--- react-project/src/components/Login.js
+++ react-project/src/components/Login.js
@@ -1,33 +1,52 @@
-import React from 'react'
-import "../Styling/Login.css"
-import { useState } from 'react'
-import { sendLoginData } from '../functions/GeneralFunctions'
+import React from "react";
+import "../Styling/Login.css";
+import { useState } from "react";
+import { sendLoginData } from "../functions/GeneralFunctions";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 export default function Login() {
+  let history = useHistory();
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState([]);
+  const [password, setPassword] = useState([]);
 
-    const [email, setEmail]= useState([]);
-    const [password, setPassword]= useState([]);
+  const userData = {
+    email,
+    password,
+  };
 
-    const userData = {
-        email,
-        password
-    }
-
-
-    return (
-        <div className="logincomp">
-            <div className="loginCont">
-                <div className="emailArea">
-                    <h3 className="emailTitle">Email</h3>
-                    <input className="logEmail" placeholder="@Email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-                </div>
-                <div className="passArea">
-                    <h3 className="emailTitle">Password</h3>
-                    <input className="logPass" placeholder="Password" value={password} onChange={(e)=> setPassword(e.target.value)}></input>
-                </div>
-                    <button class="logSub" onClick={()=> sendLoginData(userData)}>Login</button>
-                <p className="registry">Not a member? Register <a>here</a></p>
-            </div>
+  return (
+    <div className="logincomp">
+      <div className="loginCont">
+        <div className="emailArea">
+          <h3 className="emailTitle">Email</h3>
+          <input
+            className="logEmail"
+            placeholder="@Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
         </div>
-    )
+        <div className="passArea">
+          <h3 className="emailTitle">Password</h3>
+          <input
+            className="logPass"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <button
+          class="logSub"
+          onClick={() => sendLoginData(userData, history, dispatch)}
+        >
+          Login
+        </button>
+        <p className="registry">
+          Not a member? Register <a>here</a>
+        </p>
+      </div>
+    </div>
+  );
 }
--- react-project/src/functions/GeneralFunctions.js
+++ react-project/src/functions/GeneralFunctions.js
@@ -1,27 +1,43 @@
+import { LOG_IN_USER } from "../redux/action-types/ActionTypes";
 
 // ==================
 // for registering
 // ==================
 
-export const sendData = async (object) => {
-
-await fetch('http://localhost:3001/registerUser' , {
-    method: 'POST',
-    headers: { "Content-type" : "application/json"},
-    body: JSON.stringify(object)
-} )
-
-}
+export const sendData = async (object, history) => {
+  const regUser = await fetch("http://localhost:3001/registerUser", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    mode: "cors",
+    credentials: "same-origin",
+    body: JSON.stringify(object),
+  });
+  const regUserJson = await regUser.json();
+  console.log(regUser);
+  if (regUser.status === 200) {
+    history.push("/login");
+  } else {
+    window.alert("Something was wrong with your registration.", regUserJson);
+  }
+};
 
 // ===============
 // for logging in
 // ===============
 
-export const sendLoginData = async (object) => {
-    
-    await fetch('http://localhost:3001/loginUser' , {
-        method: 'GET',
-        headers: {"Content-type" : "application/json"},
-        body: JSON.stringify(object)
-    })
-}
+export const sendLoginData = async (object, history, dispatch) => {
+  const user = await fetch("http://localhost:3001/loginUser", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(object),
+  });
+  const userJson = await user.json();
+  if (user.status === 200) {
+    history.push("/markets");
+  } else {
+    window.alert("Something went wrong", userJson);
+  }
+  const userID = userJson.id;
+  console.log(userJson);
+  dispatch({ type: LOG_IN_USER, payload: userID });
+};
