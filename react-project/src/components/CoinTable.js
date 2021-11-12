90c01359e7af19233afed9c68f60597fa17ce857
Matthew Valicoff <matthewvalicoff@Matthews-MacBook-Pro.local>
2021-11-11 20:17:27 -0600

    Finished Table styling and made it mobile responsive. Changed the browser scrolling style

--- react-project/src/components/CoinTable.js
+++ react-project/src/components/CoinTable.js
@@ -1,60 +1,97 @@
-import React, {useState} from 'react'
-import { useSelector } from 'react-redux'
-import "../Styling/CoinTable.css"
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import "../Styling/CoinTable.css";
+import { formatCurrency } from "@coingecko/cryptoformat";
 
 export default function CoinTable() {
-    const coins = useSelector((state) => state.marketData)
-    const [searchValue, SetSearchValue] = useState('')
-    const cryptoFilter = coins?.data?.filter(coin => 
-        coin.name.toLowerCase().includes(searchValue.toLowerCase()));
-    const searchHandle = (e) => {SetSearchValue(e.target.value)}
+	const coins = useSelector((state) => state.marketData);
 
-    return (
-        <div className="tableContainer">
-            <input className="searchBar"onChange={searchHandle} type="search" placeholder="Lookup Crypto"/>
-            <table className="coinTable">
-                <thead className="tableHeader">
-                    <tr className="headerRow">
-                        <th className="rankHeader"><i class="fas fa-hashtag"></i></th>
-                        <th className="nameHeader"><i class="fas fa-coins"></i> Coin</th>
-                        <th className="priceHeader"><i class="fas fa-dollar-sign"></i>Price</th>
-                        <th className="per1HHeader"><i class="fas fa-arrows-alt-v"></i> 1H</th>
-                        <th className="per24Header"><i class="fas fa-arrows-alt-v"></i> 24H</th>
-                        <th className="per7DHeader"><i class="fas fa-arrows-alt-v"></i> 7D</th>
-                        <th className="volumeHeader"><i class="fas fa-balance-scale"></i>Volume</th>
-                        <th className="capHeader"><i class="fas fa-balance-scale"></i>Market Cap</th>
-                        <th className="emptyHeader"></th>
-                    </tr>
-                </thead>
-                <tbody className="tableBody">
-                    {cryptoFilter?.map((coin) => (
-                        <tr className="coinTicker">
-                            <td className="tickRank">{coin.market_cap_rank}</td>
-                            <td className="tickInfo">
-                                <div className="topInfo">
-                                    <img className="tickLogo" src={coin.image} alt="Crypto"></img>
-                                    <p className="tickSymbol">{coin?.symbol?.toUpperCase()}</p>
-                                </div>
-                                <p className="tickName">{coin.name}</p>
-                            </td>
-                            <td className="tickPrice">${coin?.current_price}</td>
-                            {coin?.price_change_percentage_1h_in_currency < 0 ? (
-                                <td className="tickHourChange red">{coin?.price_change_percentage_1h_in_currency?.toFixed(1)}%</td>) 
-                                : (<td className="tickHourChange green">{coin?.price_change_percentage_1h_in_currency?.toFixed(1)}%</td>)}
-                            {coin?.price_change_percentage_24h_in_currency < 0 ? (
-                                <td className="tick24HChange red">{coin?.price_change_percentage_24h_in_currency?.toFixed(1)}%</td>) 
-                                : (<td className="tick24HChange green">{coin?.price_change_percentage_24h_in_currency?.toFixed(1)}%</td>)}
-                            {coin?.price_change_percentage_7d_in_currency < 0 ? (
-                                <td className="tickDayChange red">{coin?.price_change_percentage_7d_in_currency?.toFixed(1)}%</td>)
-                                : (<td className="tickDayChange green">{coin?.price_change_percentage_7d_in_currency?.toFixed(1)}%</td>)}
-                            <td className="tickVolume">${coin?.total_volume?.toLocaleString()}</td>
-                            <td className="tickCap">${coin?.market_cap?.toLocaleString()}</td>
-                            <td className="buttonCell"><button className="tickButton"><i class="fas fa-plus"></i></button></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
+	const [searchValue, SetSearchValue] = useState("");
+	const searchHandle = (e) => {
+		SetSearchValue(e.target.value);
+	};
+	// This filters through the coins, formats the price on each coin, replaces coin.current price to be the new formatted price
+	const formatCrypto = (coin) => {
+		const format = coins?.data?.map((crypto) => {
+			const price = formatCurrency(crypto.current_price, "USD", "en", false);
+			let coinFormat = { ...crypto, current_price: price };
+			return coinFormat;
+		});
+		return format;
+	};
+	// Allows user to search crypto
+	const formattedCoins = formatCrypto();
+	const cryptoFilter = formattedCoins?.filter((coin) => coin.name.toLowerCase().includes(searchValue.toLowerCase()));
+
+	return (
+		<div className="tableContainer">
+			<input className="searchBar" onChange={searchHandle} type="search" placeholder="Lookup Crypto" />
+			<table className="coinTable">
+				<thead className="tableHeader">
+					<tr className="headerRow">
+						<th className="rankHeader">
+							<i class="fas fa-hashtag"></i>
+						</th>
+						<th className="nameHeader">
+							<i class="fas fa-coins"></i> Coin
+						</th>
+						<th className="priceHeader">
+							<i class="fas fa-dollar-sign"></i>Price
+						</th>
+						<th className="per1HHeader">
+							<i class="fas fa-arrows-alt-v"></i> 1H
+						</th>
+						<th className="per24Header">
+							<i class="fas fa-arrows-alt-v"></i> 24H
+						</th>
+						<th className="per7DHeader">
+							<i class="fas fa-arrows-alt-v"></i> 7D
+						</th>
+						<th className="volumeHeader">
+							<i class="fas fa-balance-scale"></i>Volume
+						</th>
+						<th className="capHeader">
+							<i class="fas fa-balance-scale"></i>Market Cap
+						</th>
+						<th className="emptyHeader"></th>
+					</tr>
+				</thead>
+				<tbody className="tableBody">
+					{cryptoFilter?.map((coin) => (
+						<tr className="coinTicker">
+							<td className="tickRank">{coin.market_cap_rank}</td>
+							<td className="tickInfo">
+								<div className="topInfo">
+									<img className="tickLogo" src={coin.image} alt="Crypto"></img>
+									<p className="tickSymbol">{coin?.symbol?.toUpperCase()}</p>
+								</div>
+								<p className="tickName">{coin.name}</p>
+							</td>
+							<td className="tickPrice">{coin.current_price}</td>
+							{coin?.price_change_percentage_1h_in_currency < 0 ? (
+								<td className="tickHourChange red">{coin?.price_change_percentage_1h_in_currency?.toFixed(1)}%</td>
+							) : (
+								<td className="tickHourChange green">{coin?.price_change_percentage_1h_in_currency?.toFixed(1)}%</td>
+							)}
+							{coin?.price_change_percentage_24h_in_currency < 0 ? (
+								<td className="tick24HChange red">{coin?.price_change_percentage_24h_in_currency?.toFixed(1)}%</td>
+							) : (
+								<td className="tick24HChange green">{coin?.price_change_percentage_24h_in_currency?.toFixed(1)}%</td>
+							)}
+							{coin?.price_change_percentage_7d_in_currency < 0 ? (
+								<td className="tickDayChange red">{coin?.price_change_percentage_7d_in_currency?.toFixed(1)}%</td>
+							) : (
+								<td className="tickDayChange green">{coin?.price_change_percentage_7d_in_currency?.toFixed(1)}%</td>
+							)}
+							<td className="tickVolume">${coin?.total_volume?.toLocaleString()}</td>
+							<td className="tickCap">${coin?.market_cap?.toLocaleString()}</td>
+							<td className="buttonCell">
+								<button className="tickButton">+</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
-
--- react-project/src/components/Markets.js
+++ react-project/src/components/Markets.js
@@ -1,42 +1,45 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
-import Coin from './Coin';
-import "../Styling/Markets.css"
-import { useState } from 'react';
-import Pagination from './Pagination';
+import React from "react";
+import { useSelector } from "react-redux";
+import Coin from "./Coin";
+import "../Styling/Markets.css";
+import { useState } from "react";
+import Pagination from "./Pagination";
 
 export default function Markets() {
-    const markets = useSelector((state) => state.marketData);
-    
-     // For Pagination
-     const [currentPage, setCurrentPage] = useState(1)
-     const [postPerPage, setPostPerPage] = useState(50)
-     const indexOfLastPost = currentPage * postPerPage;
-     const indexOfFirstPost = indexOfLastPost - postPerPage;        
-     
-     const paginate = (pageNumber) => setCurrentPage(pageNumber)
+	const markets = useSelector((state) => state.marketData);
 
-    const [searchValue, SetSearchValue] = useState('')
+	// For Pagination
+	const [currentPage, setCurrentPage] = useState(1);
+	const [postPerPage, setPostPerPage] = useState(50);
+	const indexOfLastPost = currentPage * postPerPage;
+	const indexOfFirstPost = indexOfLastPost - postPerPage;
 
-    const searchHandle = (e) => {SetSearchValue(e.target.value)}
-    const cryptoFilter = markets?.data?.filter(coin => 
-        coin.name.toLowerCase().includes(searchValue.toLowerCase()))
+	const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    const currentPosts = cryptoFilter?.slice(indexOfFirstPost, indexOfLastPost)
-        
-    return (
-        <div className="marketsPage">
-            <div className="infoArea">
-                <h1 className="infoTitle">MARKETS</h1>
-                <h4 className="greeting">Browse the markets below, click the name of the Crypto to see more on it</h4>
-            </div>
-            <div className="searchCont">
-                <input className="searchBar"onChange={searchHandle} type="search" placeholder="Lookup Crypto"/>
-            </div>
-            <div className="coinCont">
-                { currentPosts?.map((coin) => <Coin key={coin.id}coin={coin}/>)}
-            </div>
-            <Pagination postPerPage={postPerPage} totalPosts={markets?.data?.length} paginate={paginate}/>
-        </div>
-    )
+	const [searchValue, SetSearchValue] = useState("");
+
+	const searchHandle = (e) => {
+		SetSearchValue(e.target.value);
+	};
+	const cryptoFilter = markets?.data?.filter((coin) => coin.name.toLowerCase().includes(searchValue.toLowerCase()));
+
+	const currentPosts = cryptoFilter?.slice(indexOfFirstPost, indexOfLastPost);
+
+	return (
+		<div className="marketsPage">
+			<div className="infoArea">
+				<h1 className="infoTitle">MARKETS</h1>
+				<h4 className="greeting">Browse the markets below, click the name of the Crypto to see more on it</h4>
+			</div>
+			<div className="searchCont">
+				<input className="searchBar" onChange={searchHandle} type="search" placeholder="Lookup Crypto" />
+			</div>
+			<div className="coinCont">
+				{currentPosts?.map((coin) => (
+					<Coin key={coin.id} coin={coin} />
+				))}
+			</div>
+			<Pagination postPerPage={postPerPage} totalPosts={markets?.data?.length} paginate={paginate} />
+		</div>
+	);
 }
