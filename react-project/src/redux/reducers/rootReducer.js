09d73abd71eb2b37ca296d2577a70f6f6f709cb5
ZakOpry <88099668+ZakOpry@users.noreply.github.com>
2021-11-12 14:46:29 -0500

    Deleted "loginReducer". Added function to send JWT to local Storage

--- react-project/src/redux/reducers/rootReducer.js
+++ react-project/src/redux/reducers/rootReducer.js
@@ -2,12 +2,10 @@ import { combineReducers } from "redux";
 import marketData from "./MarketReducer";
 import portfolioData from "./PortfolioReducer";
 import RegistrationData from "./RegistrationReducer";
-import logInData from "./LoginReducer";
 
 const rootReducer = combineReducers({
   marketData,
   portfolioData,
   RegistrationData,
-  logInData,
 });
 export default rootReducer;
