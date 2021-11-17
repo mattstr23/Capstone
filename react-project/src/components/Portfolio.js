e0e706fd45e993bedd8982de811a04f0e5dd3842
Matthew Valicoff <matthewvalicoff@Matthews-MacBook-Pro.local>
2021-11-17 09:22:30 -0600

    very minor portfolio changes

--- react-project/src/components/Portfolio.js
+++ react-project/src/components/Portfolio.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Crypt from './Crypt';
-import "../Styling/Portfolio.css"
+import React from "react";
+import { useSelector } from "react-redux";
+import Crypt from "./Crypt";
+import "../Styling/Portfolio.css";
+import NavLogo from "../assets/NavLogo.png";
 
 export default function Portfolio() {
-    const portfolio = useSelector((state) => state.portfolioData);
-    return (
-        <div className="portfolioCont">
-            <h1 className="portfolioTitle">PORTFOLIO</h1>
-            <div className="portCrypt">
-                {portfolio?.map((crypt) => <Crypt key={crypt.id} crypt={crypt}/>)}
-            </div>
-            
-        </div>
-    )
+	const portfolio = useSelector((state) => state.portfolioData);
+	return (
+		<div className="portfolio">
+			<div className="portfolioCont">
+				<div className="portHeaderCont">
+					<img src={NavLogo} alt="logo"></img>
+					<h2>PORTFOLIO</h2>
+				</div>
+				<div className="portCrypt">
+					{portfolio?.map((crypt) => (
+						<Crypt key={crypt.id} crypt={crypt} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
