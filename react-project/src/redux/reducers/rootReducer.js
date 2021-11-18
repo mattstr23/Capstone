00f85c43958738e040f52c480ad37d918a7dbdcd
ZakOpry <88099668+ZakOpry@users.noreply.github.com>
2021-11-18 15:50:23 -0500

    working on verifiying log in token

--- react-project/src/redux/reducers/rootReducer.js
+++ react-project/src/redux/reducers/rootReducer.js
@@ -3,11 +3,13 @@ import marketData from "./MarketReducer";
 import portfolioData from "./PortfolioReducer";
 import RegistrationData from "./RegistrationReducer";
 import AccountsPageReducer from "./AccountsPageReducer";
+import verify from "./AppReducer";
 
 const rootReducer = combineReducers({
   marketData,
   portfolioData,
   RegistrationData,
   AccountsPageReducer,
+  verify,
 });
 export default rootReducer;
