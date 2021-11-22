793d48ead6daf7472e836a240efcd2eb5916082c
Matthew Valicoff <matthewvalicoff@Matthews-MacBook-Pro.local>
2021-11-22 13:41:40 -0600

    Server moved to its own repo. Final clean up of project.

--- react-project/Server/start/db.js
+++
@@ -1,6 +0,0 @@
-const pg = require("pg");
-const creds = new pg.Client(
-	"postgres://soejbsoj:g5Fj9W9_eiyCwZUo22G18RKW57XGXw0F@fanny.db.elephantsql.com/soejbsoj"
-);
-
-module.exports = creds;
--- react-project/src/functions/GeneralFunctions.js
+++ react-project/src/functions/GeneralFunctions.js
@@ -1,7 +1,4 @@
-import {
-  GET_USER_ACCOUNT_INFO,
-  LOG_IN_USER,
-} from "../redux/action-types/ActionTypes";
+import { GET_USER_ACCOUNT_INFO, LOG_IN_USER } from "../redux/action-types/ActionTypes";
 import { ADD_CRYPT } from "../redux/action-types/ActionTypes";
 
 // ==================
@@ -9,19 +6,19 @@ import { ADD_CRYPT } from "../redux/action-types/ActionTypes";
 // ==================
 
 export const sendData = async (object, history) => {
-  const regUser = await fetch("http://localhost:3001/registerUser", {
-    method: "POST",
-    headers: { "Content-type": "application/json" },
-    mode: "cors",
-    credentials: "same-origin",
-    body: JSON.stringify(object),
-  });
-  const regUserJson = await regUser.json();
-  if (regUser.status === 200) {
-    history.push("/login");
-  } else {
-    window.alert("Something was wrong with your registration.", regUserJson);
-  }
+	const regUser = await fetch("https://shielded-brook-42429.herokuapp.com/registerUser", {
+		method: "POST",
+		headers: { "Content-type": "application/json" },
+		mode: "cors",
+		credentials: "same-origin",
+		body: JSON.stringify(object),
+	});
+	const regUserJson = await regUser.json();
+	if (regUser.status === 200) {
+		history.push("/login");
+	} else {
+		window.alert("Something was wrong with your registration.", regUserJson);
+	}
 };
 
 // ===============
@@ -29,24 +26,24 @@ export const sendData = async (object, history) => {
 // ===============
 
 export const sendLoginData = async (object, history, dispatch) => {
-  const user = await fetch("http://localhost:3001/loginUser", {
-    method: "POST",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify(object),
-  });
+	const user = await fetch("https://shielded-brook-42429.herokuapp.com/loginUser", {
+		method: "POST",
+		headers: { "Content-type": "application/json" },
+		body: JSON.stringify(object),
+	});
 
-  if (user.status === 200) {
-    const userJson = await user.json();
+	if (user.status === 200) {
+		const userJson = await user.json();
 
-    let token = userJson.token;
+		let token = userJson.token;
 
-    localStorage.setItem("jsonwebtoken", token);
-    //dispatch action to set variable to true for logged in or false for not logged in
+		localStorage.setItem("jsonwebtoken", token);
+		//dispatch action to set variable to true for logged in or false for not logged in
 
-    history.push("/markets");
-  } else {
-    alert("Invalid Email And Or Password");
-  }
+		history.push("/markets");
+	} else {
+		alert("Invalid Email And Or Password");
+	}
 };
 
 // ===========================
@@ -54,18 +51,18 @@ export const sendLoginData = async (object, history, dispatch) => {
 // ===========================
 
 export const getUsersAccountInfo = async (dispatch) => {
-  const token = localStorage.getItem("jsonwebtoken");
+	const token = localStorage.getItem("jsonwebtoken");
 
-  const accountInfo = await fetch(`http://localhost:3001/account`, {
-    method: "POST",
-    headers: { authorization: `Bearer ${token}` },
-  });
+	const accountInfo = await fetch(`https://shielded-brook-42429.herokuapp.com/account`, {
+		method: "POST",
+		headers: { authorization: `Bearer ${token}` },
+	});
 
-  const accountInfoJson = await accountInfo.json();
-  dispatch({
-    type: GET_USER_ACCOUNT_INFO,
-    payload: accountInfoJson.rows[0],
-  });
+	const accountInfoJson = await accountInfo.json();
+	dispatch({
+		type: GET_USER_ACCOUNT_INFO,
+		payload: accountInfoJson.rows[0],
+	});
 };
 
 // ========================
@@ -73,15 +70,15 @@ export const getUsersAccountInfo = async (dispatch) => {
 // ========================
 
 export const buyCrypto = async (object) => {
-  const token = localStorage.getItem("jsonwebtoken");
-  const placeCrypto = await fetch("http://localhost:3001/addCrypto", {
-    method: "POST",
-    headers: {
-      authorization: `Bearer ${token}`,
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(object),
-  });
+	const token = localStorage.getItem("jsonwebtoken");
+	const placeCrypto = await fetch("https://shielded-brook-42429.herokuapp.com/addCrypto", {
+		method: "POST",
+		headers: {
+			authorization: `Bearer ${token}`,
+			"Content-type": "application/json",
+		},
+		body: JSON.stringify(object),
+	});
 };
 
 // =================
@@ -89,20 +86,20 @@ export const buyCrypto = async (object) => {
 // =================
 
 export const getUserCrypto = async (dispatch) => {
-  const token = localStorage.getItem("jsonwebtoken");
-  const usersCrypto = await fetch("http://localhost:3001/allCrypto", {
-    method: "POST",
-    headers: { authorization: `Bearer ${token}` },
-  });
-  const usersCryptoJson = await usersCrypto.json();
-  for (let x = 0; x < usersCryptoJson.rows.length; x++) {
-    const test2 = JSON.parse(usersCryptoJson.rows[x].crypto);
-    const cryptoDbId = usersCryptoJson.rows[x].id;
-    dispatch({
-      type: ADD_CRYPT,
-      payload: { ...test2, cryptoDbId: cryptoDbId },
-    });
-  }
+	const token = localStorage.getItem("jsonwebtoken");
+	const usersCrypto = await fetch("https://shielded-brook-42429.herokuapp.com/allCrypto", {
+		method: "POST",
+		headers: { authorization: `Bearer ${token}` },
+	});
+	const usersCryptoJson = await usersCrypto.json();
+	for (let x = 0; x < usersCryptoJson.rows.length; x++) {
+		const test2 = JSON.parse(usersCryptoJson.rows[x].crypto);
+		const cryptoDbId = usersCryptoJson.rows[x].id;
+		dispatch({
+			type: ADD_CRYPT,
+			payload: { ...test2, cryptoDbId: cryptoDbId },
+		});
+	}
 };
 
 // ====================
@@ -110,22 +107,22 @@ export const getUserCrypto = async (dispatch) => {
 // ====================
 
 export const deleteCrypto = async (object) => {
-  const token = localStorage.getItem("jsonwebtoken");
+	const token = localStorage.getItem("jsonwebtoken");
 
-  const deletedCrypto = await fetch("http://localhost:3001/removeCrypto", {
-    method: "POST",
-    headers: {
-      authorization: `Bearer ${token}`,
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify({ crypto: object }),
-    mode: "cors",
-    credentials: "same-origin",
-  });
+	const deletedCrypto = await fetch("https://shielded-brook-42429.herokuapp.com/removeCrypto", {
+		method: "POST",
+		headers: {
+			authorization: `Bearer ${token}`,
+			"Content-type": "application/json",
+		},
+		body: JSON.stringify({ crypto: object }),
+		mode: "cors",
+		credentials: "same-origin",
+	});
 
-  if (deletedCrypto.status === 200) {
-    window.location.reload();
-  }
+	if (deletedCrypto.status === 200) {
+		window.location.reload();
+	}
 };
 
 // ==============================
@@ -133,37 +130,37 @@ export const deleteCrypto = async (object) => {
 // ==============================
 
 export const updateUser = async (object) => {
-  const token = localStorage.getItem("jsonwebtoken");
+	const token = localStorage.getItem("jsonwebtoken");
 
-  const updateInfo = await fetch("http://localhost:3001/updateUser", {
-    method: "POST",
-    headers: {
-      authorization: `Bearer ${token}`,
-      "Content-type": "application/json",
-    },
-    mode: "cors",
-    credentials: "same-origin",
-    body: JSON.stringify(object),
-  });
+	const updateInfo = await fetch("https://shielded-brook-42429.herokuapp.com/updateUser", {
+		method: "POST",
+		headers: {
+			authorization: `Bearer ${token}`,
+			"Content-type": "application/json",
+		},
+		mode: "cors",
+		credentials: "same-origin",
+		body: JSON.stringify(object),
+	});
 
-  if (updateInfo.status === 200) {
-    window.alert("Account successfully updated!");
-  }
+	if (updateInfo.status === 200) {
+		window.alert("Account successfully updated!");
+	}
 };
 export const checkLoggedIn = async (token) => {
-  const loggedIn = await fetch("http://localhost:3001/authenticateToken", {
-    method: "POST",
-    headers: {
-      authorization: `Bearer ${token}`,
-      "Content-type": "application/json",
-    },
-    mode: "cors",
-    credentials: "same-origin",
-  });
+	const loggedIn = await fetch("https://shielded-brook-42429.herokuapp.com/authenticateToken", {
+		method: "POST",
+		headers: {
+			authorization: `Bearer ${token}`,
+			"Content-type": "application/json",
+		},
+		mode: "cors",
+		credentials: "same-origin",
+	});
 
-  if (loggedIn.status === 200) {
-    return true;
-  } else {
-    return false;
-  }
+	if (loggedIn.status === 200) {
+		return true;
+	} else {
+		return false;
+	}
 };
